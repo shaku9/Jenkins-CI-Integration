f73af2f1bd8637528306dee2cc87296a66c49571
shaku9 <146570898+shaku9@users.noreply.github.com>
2023-12-10 19:59:31 -0800

    Update ISE5F1File9.java

--- ISE5F1File9.java
+++ ISE5F1File9.java
@@ -1,5 +1,5 @@
 public class ISE5F1File9 {
     public static void main(String[] args) {
-        System.out.println("This is ISE5F1File9 and is the first integration file to be printed");
+        System.out.println("This is ISE5F1File9 and is the first integration file to be printed 5 minutes");
     }
 }
